3faf49aa27df7e4749484da3d2f5eec08675c9c5
antoniop <antoniop.silv@gmail.com>
2024-02-18 16:03:56 -0300

    feat(JsonAPI): Implementation PhotosJsonAPI Class

---
+++ app/src/main/java/br/ifsp/photos/model/PhotosJSONAPI.kt
@@ -0,0 +1,65 @@
+package br.ifsp.photos.model
+
+import android.content.Context
+import com.android.volley.NetworkResponse
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.HttpHeaderParser
+import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
+import java.net.HttpURLConnection.HTTP_NOT_MODIFIED
+import java.net.HttpURLConnection.HTTP_OK
+
+class PhotosJSONAPI(context: Context) {
+
+    companion object {
+
+        const val PHOTOS_ENDPOINT = "https://jsonplaceholder.typicode.com/photos"
+
+        @Volatile
+        private var INSTANCE: PhotosJSONAPI? = null
+
+        fun getInstance(context: Context) = INSTANCE ?: kotlin.synchronized(this) {
+            INSTANCE ?: PhotosJSONAPI(context).also {
+                INSTANCE = it
+            }
+        }
+    }
+
+    // Fila para atender todas requisições
+    private val requestQueue: RequestQueue by lazy {
+        Volley.newRequestQueue(context.applicationContext)
+    }
+
+    // Adiciona requisições na fila
+    fun <T> addToRequestQueue(request: Request<T>) {
+        requestQueue.add(request)
+    }
+
+    class PhotosListRequest(
+        private val responseListener: Response.Listener<Photos>,
+        errorListener: Response.ErrorListener
+    ): Request<Photos>(Method.GET, PHOTOS_ENDPOINT, errorListener) {
+
+        override fun parseNetworkResponse(response: NetworkResponse?): Response<Photos> =
+            if(response?.statusCode == HTTP_OK || response?.statusCode == HTTP_NOT_MODIFIED) {
+                String(response.data).run {
+                    Response.success(
+                        Gson().fromJson(this, Photos::class.java),
+                        HttpHeaderParser.parseCacheHeaders(response)
+                    )
+                }
+
+            } else {
+                Response.error(VolleyError())
+            }
+
+
+        override fun deliverResponse(response: Photos?) {
+            responseListener.onResponse(response)
+        }
+    }
+
+}
--- app/src/main/java/br/ifsp/photos/ui/MainActivity.kt
+++ app/src/main/java/br/ifsp/photos/ui/MainActivity.kt
@@ -3,10 +3,41 @@ package br.ifsp.photos.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.ifsp.photos.R
+import br.ifsp.photos.adapter.PhotosAdapter
+import br.ifsp.photos.databinding.ActivityMainBinding
+import br.ifsp.photos.model.PhotosItem
 
 class MainActivity : AppCompatActivity() {
+
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val photoslist: MutableList<PhotosItem> = mutableListOf()
+    private val photosAdapter: PhotosAdapter by lazy {
+        PhotosAdapter(this, photoslist)
+    }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(amb.root)
+
+        setSupportActionBar(amb.mainTb.apply {
+            title = "Fotos"
+        })
+
+        amb.photosSp.apply {
+            adapter = photosAdapter
+        }
     }
+
+//    private fun retrievePhotos() =
+
+
+
+
+
+
 }
