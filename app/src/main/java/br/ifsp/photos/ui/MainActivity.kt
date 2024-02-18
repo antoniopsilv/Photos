cc4bc1b9cfb9242020fcad4c4717cda85c89af18
antoniop <antoniop.silv@gmail.com>
2024-02-18 16:32:55 -0300

    feat(retrievePhotos): Implementation of the retrievePhotos method in MainActivity

--- app/src/main/java/br/ifsp/photos/ui/MainActivity.kt
+++ app/src/main/java/br/ifsp/photos/ui/MainActivity.kt
@@ -2,10 +2,12 @@ package br.ifsp.photos.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import br.ifsp.photos.R
 import br.ifsp.photos.adapter.PhotosAdapter
 import br.ifsp.photos.databinding.ActivityMainBinding
 import br.ifsp.photos.model.PhotosItem
+import br.ifsp.photos.model.PhotosJSONAPI
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,13 +33,19 @@ class MainActivity : AppCompatActivity() {
         amb.photosSp.apply {
             adapter = photosAdapter
         }
+
+        retrievePhotos()
     }
 
-//    private fun retrievePhotos() =
-
-
-
-
-
+    private fun retrievePhotos() =
+        PhotosJSONAPI.PhotosListRequest({ photoslist ->
+            photoslist.also {
+                photosAdapter.addAll(it)
+            }
+        }, {
+            Toast.makeText(this, getString(R.string.request_problem), Toast.LENGTH_SHORT).show()
+        }).also {
+            PhotosJSONAPI.getInstance(this).addToRequestQueue(it)
+        }
 
 }
